0d2cd0fea63dc66bf2a63b187007de969dcac9a6
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2026-02-16 10:50:18 +0200

    Create serializers.py

---
+++ MangaPortalAPI/accounts/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password")
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+        )
+        return user
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "display_name",
+            "bio",
+            "avatar",
+            "reputation",
+            "friends",
+        )
